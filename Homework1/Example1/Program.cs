e52b0e8b5950a1e0766c8efe4fd369943858c6c3
VladislavRazvodov <g-man-111@ya.ru>
2022-08-30 23:53:34 +0300

    Добавлено ДЗ семинара 1

---
+++ Homework1/Example1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+//Ввод
+Console.WriteLine("Введите 2 числа, программа покажет какое больше, а какое меньше ");
+Console.Write("Введите первое число ");
+int a1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int a2 = int.Parse(Console.ReadLine());
+
+//Выполняем условие
+if (a1>a2){
+    Console.WriteLine($"{a1} больше, а {a2} меньше");
+}else if (a1<a2){
+    Console.WriteLine($"{a1} меньше, а {a2} больше");
+}else if (a1==a2){
+    Console.WriteLine($"{a1} и {a2} равны");
+}
---
+++ Homework1/Example2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+//Ввод
+Console.WriteLine("Введите 3 числа, программа покажет какое максимальное");
+Console.Write("Введите первое число ");
+int a1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int a2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int a3 = int.Parse(Console.ReadLine());
+
+//Выполняем условие
+int max = a1;
+if (max < a2){ max = a2;}
+if (max < a3){ max = a3;}
+
+Console.WriteLine($"Максимальное число {max}");
