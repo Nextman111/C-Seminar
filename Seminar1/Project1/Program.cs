9ec391f96518ffdfff8dcf4263adb12a70a287f1
VladislavRazvodov <g-man-111@ya.ru>
2022-08-30 23:54:10 +0300

    Добавлены задачи с семинара 1

---
+++ Seminar1/Project1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу,
+//ввадрат числа
+
+Console.Write("Введите число ");
+int i = int.Parse(Console.ReadLine()); //приводим к целому типу введенное число
+int result = i*i;
+Console.WriteLine($"Квадрат числа {i} равен {result}");
---
+++ Seminar1/Project3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу,
+//которая будет выдавать название дня недели по заданному номеру.
+
+Console.Write("Введите первое число от 1го до 7ми");
+int a1 = int.Parse(Console.ReadLine());
+
+switch (a1){
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенине");
+        break;
+}
