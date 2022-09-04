025b58ca02803895661260a8604b4e1db9588164
VladislavRazvodov <g-man-111@ya.ru>
2022-09-04 12:57:27 +0300

    correction Solution2

--- Homework2/Solution2/Program.cs
+++ Homework2/Solution2/Program.cs
@@ -9,8 +9,12 @@ Console.WriteLine("Введите число, программа покажет 
 string number = Console.ReadLine();
 
 if (int.TryParse(number, out var input)){
-        if (number.Length > 2){
-            Console.WriteLine($"Вы ввели значение {input} . третья цифра равна {(int)(input%Math.Pow(10,number.Length-2) / Math.Pow(10,number.Length-3))}");
+        if (Math.Abs(input) > 99 ){
+            int res = 0;
+            if (input < 0 ){
+                res = 1;
+            }
+            Console.WriteLine($"Вы ввели значение {input} . третья цифра равна {(int)(Math.Abs(input)%Math.Pow(10,number.Length-2-res) / Math.Pow(10,number.Length-3-res))}");
         }
         else{
             Console.WriteLine($"Третьей цифры в числе {input} нет.");
