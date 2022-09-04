b639e19435bca3fdd98c2d252c2b7b9397ee7d5a
VladislavRazvodov <g-man-111@ya.ru>
2022-09-04 13:19:30 +0300

    add checking for negative numbers in Solution2

--- Homework2/Solution1/Program.cs
+++ Homework2/Solution1/Program.cs
@@ -7,9 +7,14 @@
 Console.WriteLine("Введите трёхзначное число, программа покажет вторую цифру этого числа.");
 string number = Console.ReadLine();
 
+char minus = ' ';
+if (number[0] == '-'){
+    number = number.Remove(0,1);
+    minus = '-';
+}
 if (number.Length == 3){
     if (int.TryParse(number, out var input)){
-        Console.WriteLine($"Вы ввели значение {input} . вторая цифра равна {input/10%10}");
+        Console.WriteLine($"Вы ввели значение {minus}{input} . вторая цифра равна {input/10%10}");
     }
     else{
         Console.WriteLine("Вы не ввели трехзначное число.");
