45d946a66c8307b8b141ec4e567fb8fe71cfad6b
VladislavRazvodov <g-man-111@ya.ru>
2022-09-10 01:17:00 +0300

    edit Solution3, add alternative solution

--- Homework3/Solution3/Program.cs
+++ Homework3/Solution3/Program.cs
@@ -4,20 +4,40 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-void CubeTable(int n){
-    if (n > 0){
-        for(int i = 1; i <= n; i++){
-            Console.WriteLine($"{i} => {Math.Pow(i,3)}");
-        }
+// void CubeTable(int n){
+//     if (n > 0){
+//         for(int i = 1; i <= n; i++){
+//             Console.WriteLine($"{i} => {Math.Pow(i,3)}");
+//         }
+//     }
+//     else{
+//         Console.WriteLine("Некорректное значение");
+//     }
+// }
+
+int[] CubeTable(int n){
+    int[] res = new int[n];
+    for(int i = 0; i < res.Length; i++){
+        res[i] = (int)Math.Pow((i+1), 3);
     }
-    else{
-        Console.WriteLine("Некорректное значение");
+    return res;
+    
+}
+
+void ConsoleTable(int[] table){
+    for(int i = 0; i < table.Length; i++ ){
+        Console.WriteLine($"{i+1} => {table[i]}");
     }
 }
 
 Console.WriteLine("Введите натуральное число N, программа выдаст таблицу кубов натуральных чисел от 1 до N");
 if (int.TryParse(Console.ReadLine(), out int n)){
-    CubeTable(n);
+    if (n > 0){
+        ConsoleTable(CubeTable(n));
+    }
+    else{
+        Console.WriteLine("Некорректное значение");
+    }
 }
 else{
     Console.WriteLine("Некорректное значение");
